c400beaec0213276d123590d1557e4ed6aef7004
riguelbf <riguel@dkosoftware.com.br>
2017-03-14 23:26:07 -0300

    bootstrap finished

--- src/frontend/webpack.config.js
+++ src/frontend/webpack.config.js
@@ -2,7 +2,7 @@ const webpack = require('webpack');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
-    entry: './../index.jsx',
+    entry: './../frontend/src/index.jsx',
     output: {
         path: __dirname + '/public',
         filename: './app.js'
@@ -20,9 +20,9 @@ module.exports = {
     plugins: [
         new ExtractTextPlugin('app.css')
     ],
-    module {
+    module: {
         loaders: [{
-                test: /;js[x]?$/,
+                test: /.js[x]?$/,
                 loader: 'babel-loader',
                 exclude: './node_modules',
                 query: {
@@ -35,8 +35,24 @@ module.exports = {
                 loader: ExtractTextPlugin.extract('style-loader', 'css-loader')
             },
             {
-                test: /\.woff|.woff2|.ttf|.eot|./svg * .*$ / ,
-                loader: 'file'
+                test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,
+                loader: "url?limit=10000&mimetype=application/font-woff"
+            },
+            {
+                test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/,
+                loader: "url?limit=10000&mimetype=application/font-woff"
+            },
+            {
+                test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+                loader: "url?limit=10000&mimetype=application/octet-stream"
+            },
+            {
+                test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+                loader: "file"
+            },
+            {
+                test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+                loader: "url?limit=10000&mimetype=image/svg+xml"
             }
         ]
 
