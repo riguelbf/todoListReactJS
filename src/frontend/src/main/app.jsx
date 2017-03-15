dc968f03064239af25528462e89ab5392420162f
riguelbf <riguel@dkosoftware.com.br>
2017-03-14 23:41:26 -0300

    component in class

--- src/frontend/src/main/app.jsx
+++ src/frontend/src/main/app.jsx
@@ -1,10 +1,23 @@
 import 'modules/bootstrap/dist/css/bootstrap.min.css'
 import 'modules/font-awesome/css/font-awesome.min.css'
 
-import React from 'react'
+import React, { Component } from 'react'
 
-export default props => (
-    <div class='container'>
-        <h1>Test ok!</h1>
-    </div>
-)
+
+export default class App extends React.Component{
+
+    constructor(props) {
+        super(props);
+    }
+
+    render(){
+       return (
+        <div class='container'>
+            <h1>Test ok!</h1>
+            <button className='btn-primary'>test </button>     
+        </div>
+       )
+    }
+} 
+
+
