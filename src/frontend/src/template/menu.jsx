7061bf9a824136fb1de522325ba4d19600ca9b4b
riguelbf <riguel@dkosoftware.com.br>
2017-03-17 01:05:36 -0300

    correction menu/navbar

--- src/frontend/src/template/menu.jsx
+++ src/frontend/src/template/menu.jsx
@@ -2,15 +2,15 @@ import React , { Component } from 'react';
 
 module.exports = props => (
     <nav className='navbar navbar-inverse bg-inverse'>
-        <div className='container'>
-            <div className='navba-header'>
-                <a href="#" className='navba-brand'>
-                    <i className='fa fa-calendar-check-o'></i>TodoApp       
+        <div className='container-fluid'>
+            <div className='navbar-header'>
+                <a href="#" className='navbar-brand'>
+                    <i className='fa fa-calendar-check-o'></i> TodoApp       
                 </a>
             </div>
         </div>
-        <div id='navbar' className='navbar-collapse collapse'>
-            <ul className='navbar navbar-nav'>
+        <div id='navbar' className='collapse navbar-collapse'>
+            <ul className='nav navbar-nav'>
                 <li>
                     <a href="#/todos">Todo</a>
                 </li>
