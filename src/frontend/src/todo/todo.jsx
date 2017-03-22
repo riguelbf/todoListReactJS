ea0fcc5eeca4c3a4b4e00b4607ee01a1fd555fce
riguelbf <riguel@dkosoftware.com.br>
2017-03-21 22:47:23 -0300

    include handleAdd event

--- src/frontend/src/todo/todo.jsx
+++ src/frontend/src/todo/todo.jsx
@@ -4,11 +4,21 @@ import TodoForm from './../todo/todoForm';
 import TodoList from './../todo/totoList';
 
 class Todo extends Component {
+
+    constructor(props) {
+        super(props);
+        this.handleAdd = this.handleAdd.bind(this);
+    }
+
+    handleAdd() {
+        console.log(this);
+    }
+
     render() {
         return (
             <div className='container'>
                 <PageHeader name='Todo' small='task' />
-                <TodoForm />
+                <TodoForm handleAdd={this.handleAdd} />
                 <TodoList />
             </div>
         );
--- src/frontend/src/todo/todoForm.jsx
+++ src/frontend/src/todo/todoForm.jsx
@@ -1,16 +1,16 @@
 import React, { Component } from 'react';
+import IconButton from './../template/button';
 
 class TodoForm extends Component {
+
     render() {
         return (
             <div role='form' className='todoForm' >
                 <div className='col-xs-12 col-sm-9 col-md-10'>
-                    <input id='description' className='form-control'  placeholder='Adicione uma tarefa' type="text"/>
+                    <input id='description' className='form-control' placeholder='Adicione uma tarefa' type="text" />
                 </div>
                 <div className='col-xs-12 col-sm-3 col-md-2'>
-                    <button className='btn-primary'>
-                        <i className='fa fa-plus'></i>
-                    </button>
+                    <IconButton style='primary' onClick={this.props.handleAdd} icon='plus' />
                 </div>
             </div>
         );
